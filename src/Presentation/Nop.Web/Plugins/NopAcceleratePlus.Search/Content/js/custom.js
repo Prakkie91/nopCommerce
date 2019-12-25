8700bcca6573c0d0e5732678bccbcf4285eaa4d7
Prakkie91 <enricowillemse.was@gmail.com>
2019-12-25 10:47:02 +0800

    Update

---
+++ src/Presentation/Nop.Web/Plugins/NopAcceleratePlus.Search/Content/js/custom.js
@@ -0,0 +1,28 @@
+﻿function Customfunction() {
+    //$.getScript("/Plugins/SevenSpikes.Nop.Plugins.QuickView/Scripts/QuickView.min.js", function () {
+    //    // do something here after script has loaded
+    //});
+
+    //$.getScript("/Plugins/SevenSpikes.Core/Scripts/cloudzoom.core.min.js", function () {
+    //    // do something here after script has loaded
+    //});
+}
+
+//1 If you are using the NopTemplate Theme please put this code inside condition:
+//$(this).siblings('.filtersGroupPanel').slideToggle('slow');
+//$('.title').show();
+
+//2 If you are not using any nop Template theme, then comment this code
+//var isAjax = $("#nopAccplus-facetdata").attr("data-ajaxrequest");
+//if (screen.width >= 1025 || isAjax == "True") {}
+
+$(document).on("click", ".filter-block .title", toggleFacet);
+
+function toggleFacet() {
+    var isAjax = $("#nopAccplus-facetdata").attr("data-ajaxrequest");
+    if (screen.width >= 1025 || isAjax == "True") {
+        $(this).siblings('.filtersGroupPanel').slideToggle('slow');
+        $('.title').show();
+    }
+    $(this).find('.acc_icon_expand').toggleClass('rotate');
+}
